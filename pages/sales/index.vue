b809222ce658d795cdba13f1d99be6482357bf18
Egor Dorosh <egor.dorosh@libertexgroup.com>
2022-07-18 12:49:24 +0200

    Add error page. Add sales routes

---
+++ pages/sales/index.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <Fragment>
+    <article>
+      <section>
+        <h1>
+          Last sales
+        </h1>
+      </section>
+    </article>
+  </Fragment>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
