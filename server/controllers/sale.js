095e7a57763d30cc19508425ce03d0cf8b15bc7c
Egor Dorosh <egor.dorosh@libertexgroup.com>
2022-07-19 19:03:27 +0200

    Add sale form

--- server/controllers/sale.js
+++ server/controllers/sale.js
@@ -1,4 +1,7 @@
+const { validationResult } = require('express-validator');
+
 const Sale = require('../models/sale');
+const User = require('../models/user');
 
 exports.getSales = async(req, res, next) => {
   try {
@@ -8,6 +11,74 @@ exports.getSales = async(req, res, next) => {
       message: 'Fetched sales successfully.',
       sales,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.getUserSales = async(req, res, next) => {
+  try {
+    const sales = await Sale.find({ owner: req.userId }).populate('products').populate('owner', '-password');
+
+    res.status(200).json({
+      message: 'Fetched user sales successfully.',
+      sales,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.createSale = async(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation faild.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    next(error);
+    return;
+  }
+
+  const user = await User.findById(req.userId);
+
+  try {
+    const owner = req.userId;
+    const saleIsExist = await Sale.countDocuments({ id: owner });
+    let sale;
+    const description = req.body.description;
+    const isPublished = req.body.isPublished;
+
+    if (saleIsExist === 0) {
+      sale = new Sale({
+        id: owner,
+        description,
+        isPublished,
+        owner,
+        products: []
+      });
+      await sale.save();
+      user.sales.push(sale);
+      await user.save();
+      res.status(201).json({
+        message: 'Sale created successful.',
+        sale,
+      });
+    } else {
+      sale = await Sale.findOne({ id: owner });
+      sale.description = description;
+      sale.isPublished = isPublished;
+      sale.save();
+      res.status(200).json({
+        message: 'Sale successfuly updated',
+        sale,
+      });
+    }
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
--- store/sale/actions.js
+++ store/sale/actions.js
@@ -4,10 +4,49 @@ const actions = {
       const response = await fetch(`${process.env.protocol}://${process.env.hostName}/api-sales`);
       const status = response.status;
       const data = await response.json();
-      console.log(data);
 
       if (status === 200) {
         context.commit('setSales', data.sales);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  async fetchUserSales(context) {
+    try {
+      const response = await fetch(`${process.env.protocol}://${process.env.hostName}/api-user-sales`, {
+        headers: {
+          Authorization: 'Bearer ' + context.rootGetters['user/getToken'],
+        },
+      });
+      const status = response.status;
+      const data = await response.json();
+
+      if (status === 200) {
+        context.commit('setUserSales', data.sales);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  async saveSale(context, saleData) {
+    try {
+      const response = await fetch(`${process.env.protocol}://${process.env.hostName}/api-sales`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + context.rootGetters['user/getToken'],
+        },
+        body: JSON.stringify(saleData),
+      });
+      const status = response.status;
+      const data = await response.json();
+      data.status = status;
+
+      if (status === 200 || status === 201) {
+        context.commit('setUserSales', [data.sale]);
       }
     } catch (error) {
       throw new Error(error);
